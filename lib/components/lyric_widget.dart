4485f9d0c1c36bb7712739c2c64e65ae43a43e25
贺宇成 <heyucheng001@ke.com>
2020-03-29 12:47:36 +0800

    [Add]: [Lyric seekTo]

--- lib/components/lyric_widget.dart
+++ lib/components/lyric_widget.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
@@ -5,12 +6,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_music_plugin/music.dart';
 import 'package:music/entity/bean/lyric.dart';
 import 'package:music/provider/play_songs_model.dart';
+import 'package:music/util/date_util.dart';
 import 'package:provider/provider.dart';
 
 class LyricWidget extends StatefulWidget {
   final screenCount;
 
-  LyricWidget({this.screenCount = 12});
+  LyricWidget({this.screenCount = 13});
 
   @override
   State<StatefulWidget> createState() => LyricState();
@@ -22,6 +24,10 @@ class LyricState extends State<LyricWidget> {
   List<Lyric> lyricList;
   ScrollController controller;
   int _position = 0;
+  bool isMove = false;
+  Timer dragEndTimer; // 拖动结束任务
+  int preSelectPosition = -1;
+  Duration curTimeBarDuration = Duration(milliseconds: 0);
 
   @override
   void initState() {
@@ -49,9 +55,10 @@ class LyricState extends State<LyricWidget> {
         assert(model.curSongInfo != null);
         if (_songId != model.curSongInfo.hash) {
           lyricList = Lyric.formatLyric(model.curSongInfo.lyrics);
-          lyricList.forEach((d) {
-            print('歌词+ $d');
-          });
+          isMove = false;
+          dragEndTimer?.cancel();
+          dragEndTimer = null;
+          preSelectPosition = -1;
         }
         return StreamBuilder<MusicState>(
           initialData: model.musicStateInfo,
@@ -60,19 +67,107 @@ class LyricState extends State<LyricWidget> {
             if (data.hasData) {
               computePosition(data.data.position);
             }
-            return Container(
-              height: itemHeight * widget.screenCount,
-              child: ListView.builder(
-                controller: controller,
-                padding: EdgeInsets.zero,
-                itemBuilder: _buildLyricItem,
-                itemCount: lyricList.length + widget.screenCount,
-              ),
-            );
+            return _buildLyricList();
           },
         );
       },
     );
+  }
+
+  Widget _buildLyricList() {
+    return Container(
+        height: itemHeight * widget.screenCount,
+        child: Listener(
+            onPointerUp: (event) {
+              recoverMoveState();
+            },
+            onPointerCancel: (event) {
+              recoverMoveState();
+            },
+            onPointerDown: (event) {
+              preSelectPosition = -1;
+              dragEndTimer?.cancel();
+            },
+            onPointerMove: (event) {
+              isMove = true;
+              int curPosition =
+                  (controller.offset + itemHeight ~/ 2) ~/ itemHeight;
+              if (curPosition != preSelectPosition) {
+                curTimeBarDuration = lyricList[curPosition].startTime;
+                preSelectPosition = curPosition;
+                if (mounted) {
+                  setState(() {});
+                }
+              }
+            },
+            child: Stack(
+              children: <Widget>[
+                ListView.builder(
+                  controller: controller,
+                  padding: EdgeInsets.zero,
+                  itemBuilder: _buildLyricItem,
+                  itemCount: lyricList.length + widget.screenCount - 1,
+                ),
+                _buildLyricTimeBar()
+              ],
+            )));
+  }
+
+  Widget _buildLyricTimeBar() {
+    return Container(
+      alignment: Alignment.center,
+      child: isMove
+          ? SizedBox(
+              height: itemHeight,
+              child: Row(
+                children: <Widget>[
+                  GestureDetector(
+                    child: Icon(
+                      Icons.play_arrow,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                    onTap: () {
+                      MusicWrapper.singleton
+                          .seekTo(curTimeBarDuration.inMilliseconds);
+                    },
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: Divider(
+                      height: 1,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    DateUtil.formatDateMs(curTimeBarDuration.inMilliseconds,
+                        format: "mm:ss"),
+                    style: TextStyle(color: Colors.white70, fontSize: 16),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  )
+                ],
+              ),
+            )
+          : null,
+    );
+  }
+
+  void recoverMoveState() {
+    dragEndTimer?.cancel();
+    dragEndTimer = null;
+    dragEndTimer = Timer(const Duration(milliseconds: 2000), () {
+      if (isMove && mounted) {
+        setState(() {
+          isMove = false;
+        });
+      }
+    });
   }
 
   /// 计算当前中心歌词位置
@@ -82,6 +177,7 @@ class LyricState extends State<LyricWidget> {
       _position = position;
       final offset = position * itemHeight;
       if (controller?.hasClients == true) {
+        if (isMove) return;
         controller.animateTo(offset,
             duration: Duration(milliseconds: 200), curve: Curves.easeInOut);
       } else {
@@ -91,8 +187,8 @@ class LyricState extends State<LyricWidget> {
   }
 
   Widget _buildLyricItem(context, index) {
-    if (index < widget.screenCount / 2 ||
-        index >= lyricList.length + widget.screenCount / 2) {
+    if (index < widget.screenCount ~/ 2 ||
+        index >= lyricList.length + widget.screenCount ~/ 2) {
       // 占位空行
       return Container(height: itemHeight);
     }
