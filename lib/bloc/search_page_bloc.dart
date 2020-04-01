1927542528fb22db45f43b38fd8d0dfa34ae8f66
贺宇成 <heyucheng001@ke.com>
2020-04-01 10:19:12 +0800

    [Change]: [添加防抖动防护]

--- lib/bloc/search_page_bloc.dart
+++ lib/bloc/search_page_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:music/bloc/base_bloc.dart';
 import 'package:music/entity/association_entity.dart';
 import 'package:music/entity/hot_search_entity.dart';
@@ -6,11 +8,23 @@ import 'package:music/api/api_url.dart';
 
 /// 搜索页面Bloc
 class SearchPageBloc with ResponseWorker {
+  Timer _timer;
+
   void fetchAssociationData(String keyword) {
+    _timer?.cancel();
+    _timer = null;
+    _timer = Timer(const Duration(milliseconds: 300), () {
+      fetchAssociationDataInternal(keyword);
+    });
+  }
+
+  void fetchAssociationDataInternal(String keyword) {
+    print("fetchAssociationDataInternal :$keyword");
     if (keyword?.isEmpty ?? true) {
       //发送一个空数据清空之前的联想词
       streamManager.addDataToSinkByKey(AssociationEntity,
           PageData.complete(AssociationEntity()..status = 1));
+      return;
     }
     dealResponse<AssociationEntity>(
         responseProvider: () =>
