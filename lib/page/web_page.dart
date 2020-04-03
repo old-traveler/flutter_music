e7531ec3e090f220f2b4e49f7b83c3582647e124
贺宇成 <heyucheng001@ke.com>
2020-04-03 10:07:04 +0800

    [Add]: [live support]

--- lib/page/web_page.dart
+++ lib/page/web_page.dart
@@ -12,20 +12,79 @@ class WebPage extends StatefulWidget {
 }
 
 class WebState extends State<WebPage> {
+  MethodChannel _channel;
+  String _title = '';
+
   @override
   Widget build(BuildContext context) {
     print("url  ${widget.url}");
     return Scaffold(
       appBar: AppBar(
-        title: Text('酷狗直播'),
+        title: Text(_title),
+        leading: GestureDetector(
+          child: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onTap: () => backOrPop(),
+        ),
+        actions: <Widget>[
+          GestureDetector(
+            child: Icon(
+              Icons.refresh,
+              color: Colors.white,
+            ),
+            onTap: reloadUrl,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          GestureDetector(
+            child: Icon(
+              Icons.close,
+              color: Colors.white,
+            ),
+            onTap: () => Navigator.of(context).pop(),
+          ),
+          SizedBox(
+            width: 14,
+          ),
+        ],
       ),
       body: AndroidView(
         viewType: 'flutterWebView',
         creationParams: {
           'url': widget.url,
         },
+        onPlatformViewCreated: onPlatformViewCreated,
         creationParamsCodec: const StandardMessageCodec(),
       ),
     );
+  }
+
+  void backOrPop() async {
+    bool isBacked = await _channel.invokeMethod('back');
+    if (!isBacked) {
+      Navigator.pop(context);
+    }
+  }
+
+  void reloadUrl() {
+    _channel.invokeMethod('reload');
+  }
+
+  void onPlatformViewCreated(int id) {
+    _channel = MethodChannel('web_view_plugin');
+    _channel.setMethodCallHandler(webViewMethodHandler);
+  }
+
+  Future<dynamic> webViewMethodHandler(MethodCall call) async {
+    if (call.method == 'onReceivedTitle') {
+      setState(() {
+        _title = call.arguments;
+      });
+      return true;
+    }
+    return null;
   }
 }
